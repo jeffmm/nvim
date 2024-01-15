bf9bf582a6d2527e7f1a5211f6a16b1f99537e20
Jeff Moore <jmoore@manifold.ai>
2024-01-15 11:14:57 -0700

    feat: don't rock the boat

---
+++ lua/plugins/notebook-navigator.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "GCBallesteros/NotebookNavigator.nvim",
+    keys = {
+      {
+        "]h",
+        function()
+          require("notebook-navigator").move_cell("d")
+        end,
+      },
+      {
+        "[h",
+        function()
+          require("notebook-navigator").move_cell("u")
+        end,
+      },
+      { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+      { "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+    },
+    dependencies = {
+      "echasnovski/mini.comment",
+      "hkupty/iron.nvim", -- repl provider
+      -- "akinsho/toggleterm.nvim", -- alternative repl provider
+      -- "benlubas/molten-nvim", -- alternative repl provider
+      "anuvyklack/hydra.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      local nn = require("notebook-navigator")
+      nn.setup({ activate_hydra_keys = "<leader>h" })
+    end,
+  },
+
+  {
+    "echasnovski/mini.hipatterns",
+    event = "VeryLazy",
+    dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
+    opts = function()
+      local nn = require("notebook-navigator")
+
+      local opts = { highlighters = { cells = nn.minihipatterns_spec } }
+      return opts
+    end,
+  },
+
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
+    opts = function()
+      local nn = require("notebook-navigator")
+
+      local opts = { custom_textobjects = { h = nn.miniai_spec } }
+      return opts
+    end,
+  },
+}
