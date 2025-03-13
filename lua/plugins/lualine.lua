2e0856accc3f947b52fd7f2b6d9bb756733c33ab
Jeff Moore <jeffreymm@protonmail.com>
2025-03-13 10:02:47 -0600

    fix: lualine deprecated get_active_clients

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   init = function()
     -- disable until lualine loads
     vim.opt.laststatus = 0
@@ -9,21 +9,21 @@ return {
   opts = function()
     -- miasma colors
     local colors = {
-      bg = "#eff1f5",
-      black = "#ffffff",
-      grey = "#9ca0b0",
-      red = "#d20f39",
-      green = "#40a02b",
-      yellow = "#df8e1d",
-      blue = "#1e66f5",
-      magenta = "#ea76cb",
-      cyan = "#04a5e5",
-      white = "#ffffff",
+      bg = '#eff1f5',
+      black = '#ffffff',
+      grey = '#9ca0b0',
+      red = '#d20f39',
+      green = '#40a02b',
+      yellow = '#df8e1d',
+      blue = '#1e66f5',
+      magenta = '#ea76cb',
+      cyan = '#04a5e5',
+      white = '#ffffff',
     }
 
     local conditions = {
       buffer_not_empty = function()
-        return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+        return vim.fn.empty(vim.fn.expand '%:t') ~= 1
       end,
       hide_in_width_first = function()
         return vim.fn.winwidth(0) > 80
@@ -32,8 +32,8 @@ return {
         return vim.fn.winwidth(0) > 70
       end,
       check_git_workspace = function()
-        local filepath = vim.fn.expand("%:p:h")
-        local gitdir = vim.fn.finddir(".git", filepath .. ";")
+        local filepath = vim.fn.expand '%:p:h'
+        local gitdir = vim.fn.finddir('.git', filepath .. ';')
         return gitdir and #gitdir > 0 and #gitdir < #filepath
       end,
     }
@@ -42,13 +42,13 @@ return {
       n = colors.red,
       i = colors.green,
       v = colors.blue,
-      [""] = colors.blue,
+      [''] = colors.blue,
       V = colors.blue,
       c = colors.magenta,
       no = colors.red,
       s = colors.orange,
       S = colors.orange,
-      [""] = colors.orange,
+      [''] = colors.orange,
       ic = colors.yellow,
       R = colors.yellow,
       Rv = colors.yellow,
@@ -56,16 +56,16 @@ return {
       ce = colors.yellow,
       r = colors.cyan,
       rm = colors.cyan,
-      ["r?"] = colors.cyan,
-      ["!"] = colors.red,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.red,
       t = colors.red,
     }
     -- config
     local config = {
       options = {
         -- remove default sections and component separators
-        component_separators = "",
-        section_separators = "",
+        component_separators = '',
+        section_separators = '',
         theme = {
           -- setting defaults to statusline
           normal = { c = { fg = colors.fg, bg = colors.bg } },
@@ -116,13 +116,13 @@ return {
 
     -- dump object contents
     local function dump(o)
-      if type(o) == "table" then
-        local s = ""
+      if type(o) == 'table' then
+        local s = ''
         for k, v in pairs(o) do
-          if type(k) ~= "number" then
+          if type(k) ~= 'number' then
             k = '"' .. k .. '"'
           end
-          s = s .. dump(v) .. ","
+          s = s .. dump(v) .. ','
         end
         return s
       else
@@ -131,169 +131,169 @@ return {
     end
 
     -- active left section
-    active_left({
+    active_left {
       function()
         local icon
-        local ok, devicons = pcall(require, "nvim-web-devicons")
+        local ok, devicons = pcall(require, 'nvim-web-devicons')
         if ok then
-          icon = devicons.get_icon(vim.fn.expand("%:t"))
+          icon = devicons.get_icon(vim.fn.expand '%:t')
           if icon == nil then
             icon = devicons.get_icon_by_filetype(vim.bo.filetype)
           end
         else
-          if vim.fn.exists("*WebDevIconsGetFileTypeSymbol") > 0 then
+          if vim.fn.exists '*WebDevIconsGetFileTypeSymbol' > 0 then
             icon = vim.fn.WebDevIconsGetFileTypeSymbol()
           end
         end
         if icon == nil then
-          icon = ""
+          icon = ''
         end
-        return icon:gsub("%s+", "")
+        return icon:gsub('%s+', '')
       end,
       color = function()
         return { bg = mode_color[vim.fn.mode()], fg = colors.white }
       end,
       padding = { left = 1, right = 1 },
-      separator = { right = "▓▒░" },
-    })
-    active_left({
-      "filename",
+      separator = { right = '▓▒░' },
+    }
+    active_left {
+      'filename',
       cond = conditions.buffer_not_empty,
       color = function()
         return { bg = mode_color[vim.fn.mode()], fg = colors.white }
       end,
       padding = { left = 1, right = 1 },
-      separator = { right = "▓▒░" },
+      separator = { right = '▓▒░' },
       symbols = {
-        modified = "󰶻 ",
-        readonly = " ",
-        unnamed = " ",
-        newfile = " ",
+        modified = '󰶻 ',
+        readonly = ' ',
+        unnamed = ' ',
+        newfile = ' ',
       },
-    })
-    active_left({
-      "branch",
-      icon = "",
+    }
+    active_left {
+      'branch',
+      icon = '',
       color = { bg = colors.blue, fg = colors.black },
       padding = { left = 0, right = 1 },
-      separator = { right = "▓▒░", left = "░▒▓" },
-    })
+      separator = { right = '▓▒░', left = '░▒▓' },
+    }
 
     -- inactive left section
-    inactive_left({
+    inactive_left {
       function()
-        return ""
+        return ''
       end,
       cond = conditions.buffer_not_empty,
       color = { bg = colors.black, fg = colors.grey },
       padding = { left = 1, right = 1 },
-    })
-    inactive_left({
-      "filename",
+    }
+    inactive_left {
+      'filename',
       cond = conditions.buffer_not_empty,
       color = { bg = colors.black, fg = colors.grey },
       padding = { left = 1, right = 1 },
-      separator = { right = "▓▒░" },
+      separator = { right = '▓▒░' },
       symbols = {
-        modified = "",
-        readonly = "",
-        unnamed = "",
-        newfile = "",
+        modified = '',
+        readonly = '',
+        unnamed = '',
+        newfile = '',
       },
-    })
+    }
 
     -- active right section
-    active_right({
+    active_right {
       function()
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         local clients_list = {}
         for _, client in pairs(clients) do
           if not clients_list[client.name] then
             table.insert(clients_list, client.name)
           end
         end
-        local lsp_lbl = dump(clients_list):gsub("(.*),", "%1")
-        return lsp_lbl:gsub(",", ", ")
+        local lsp_lbl = dump(clients_list):gsub('(.*),', '%1')
+        return lsp_lbl:gsub(',', ', ')
       end,
-      icon = " ",
+      icon = ' ',
       color = { bg = colors.green, fg = colors.black },
       padding = { left = 1, right = 1 },
       cond = conditions.hide_in_width_first,
-      separator = { right = "▓▒░", left = "░▒▓" },
-    })
+      separator = { right = '▓▒░', left = '░▒▓' },
+    }
 
-    active_right({
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      symbols = { error = " ", warn = " ", info = " " },
+    active_right {
+      'diagnostics',
+      sources = { 'nvim_diagnostic' },
+      symbols = { error = ' ', warn = ' ', info = ' ' },
       colored = false,
       color = { bg = colors.magenta, fg = colors.black },
       padding = { left = 1, right = 1 },
-      separator = { right = "▓▒░", left = "░▒▓" },
-    })
-    active_right({
-      "searchcount",
+      separator = { right = '▓▒░', left = '░▒▓' },
+    }
+    active_right {
+      'searchcount',
       color = { bg = colors.cyan, fg = colors.black },
       padding = { left = 1, right = 1 },
-      separator = { right = "▓▒░", left = "░▒▓" },
-    })
-    active_right({
-      "location",
+      separator = { right = '▓▒░', left = '░▒▓' },
+    }
+    active_right {
+      'location',
       color = { bg = colors.red, fg = colors.white },
       padding = { left = 1, right = 0 },
-      separator = { left = "░▒▓" },
-    })
-    active_right({
+      separator = { left = '░▒▓' },
+    }
+    active_right {
       function()
-        local cur = vim.fn.line(".")
-        local total = vim.fn.line("$")
-        return string.format("%2d%%%%", math.floor(cur / total * 100))
+        local cur = vim.fn.line '.'
+        local total = vim.fn.line '$'
+        return string.format('%2d%%%%', math.floor(cur / total * 100))
       end,
       color = { bg = colors.red, fg = colors.white },
       padding = { left = 1, right = 1 },
       cond = conditions.hide_in_width,
-      separator = { right = "▓▒░" },
-    })
-    active_right({
-      "o:encoding",
+      separator = { right = '▓▒░' },
+    }
+    active_right {
+      'o:encoding',
       fmt = string.upper,
       cond = conditions.hide_in_width,
       padding = { left = 1, right = 1 },
       color = { bg = colors.blue, fg = colors.black },
-    })
-    active_right({
-      "fileformat",
+    }
+    active_right {
+      'fileformat',
       fmt = string.lower,
       icons_enabled = false,
       cond = conditions.hide_in_width,
       color = { bg = colors.blue, fg = colors.black },
-      separator = { right = "▓▒░" },
+      separator = { right = '▓▒░' },
       padding = { left = 0, right = 1 },
-    })
+    }
 
     -- inactive right section
-    inactive_right({
-      "location",
+    inactive_right {
+      'location',
       color = { bg = colors.black, fg = colors.grey },
       padding = { left = 1, right = 0 },
-      separator = { left = "░▒▓" },
-    })
-    inactive_right({
-      "progress",
+      separator = { left = '░▒▓' },
+    }
+    inactive_right {
+      'progress',
       color = { bg = colors.black, fg = colors.grey },
       cond = conditions.hide_in_width,
       padding = { left = 1, right = 1 },
-      separator = { right = "▓▒░" },
-    })
-    inactive_right({
-      "fileformat",
+      separator = { right = '▓▒░' },
+    }
+    inactive_right {
+      'fileformat',
       fmt = string.lower,
       icons_enabled = false,
       cond = conditions.hide_in_width,
       color = { bg = colors.black, fg = colors.grey },
-      separator = { right = "▓▒░" },
+      separator = { right = '▓▒░' },
       padding = { left = 0, right = 1 },
-    })
+    }
     --
     return config
   end,
